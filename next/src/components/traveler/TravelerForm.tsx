4f4d439cf2e5b672c34119be7c3ea375e7b2e616
marcos-sco <marcos_sco@outlook.com>
2026-06-06 18:16:41 -0300

    feat: add TravelerForm component for managing traveler details

---
+++ next/src/components/traveler/TravelerForm.tsx
@@ -0,0 +1,63 @@
+import { QuoteRequest } from "@/types/quote";
+import { UseFormRegister } from "react-hook-form";
+
+interface TravelerFormProps {
+  index: number;
+  remove: () => void;
+  register: UseFormRegister<QuoteRequest>;
+}
+
+export default function TravelerForm({
+  index,
+  remove,
+  register,
+}: TravelerFormProps) {
+  return (
+    <div className="border rounded-lg p-4 space-y-4">
+      <h3 className="font-semibold">Traveler #{index + 1}</h3>
+
+      <div>
+        <label>Nome</label>
+
+        <input
+          className="w-full border rounded px-3 py-2"
+          {...register(`travelers.${index}.name`)}
+        />
+      </div>
+
+      <div>
+        <label>Data de nascimento</label>
+
+        <input
+          type="date"
+          className="w-full border rounded px-3 py-2"
+          {...register(`travelers.${index}.birth_date`)}
+        />
+      </div>
+
+      <div className="space-y-2">
+        <label className="flex gap-2">
+          <input
+            type="checkbox"
+            value="BAGAGEM"
+            {...register(`travelers.${index}.additionals`)}
+          />
+          Bagagem
+        </label>
+
+        <label className="flex gap-2">
+          <input
+            type="checkbox"
+            value="ESPORTES_AVENTURA"
+            {...register(`travelers.${index}.additionals`)}
+          />
+          Esportes de Aventura
+        </label>
+      </div>
+
+      <button type="button" onClick={remove} className="text-red-500">
+        Remover Viajante
+      </button>
+    </div>
+  );
+}
