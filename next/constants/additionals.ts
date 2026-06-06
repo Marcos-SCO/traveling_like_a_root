1a1c12aba07230788b47e58b880a776d54b1c5cd
marcos-sco <marcos_sco@outlook.com>
2026-06-06 17:24:02 -0300

    feat: add ADDITIONALS and DESTINATIONS constants for travel options

---
+++ next/constants/additionals.ts
@@ -0,0 +1,4 @@
+export const ADDITIONALS = [
+  "BAGAGEM",
+  "ESPORTES_AVENTURA",
+] as const;
---
+++ next/constants/destinations.ts
@@ -0,0 +1,5 @@
+export const DESTINATIONS = [
+  "NACIONAL",
+  "AMERICAS",
+  "EUROPA",
+] as const;
