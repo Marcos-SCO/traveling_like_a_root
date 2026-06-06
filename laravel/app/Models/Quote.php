8ca66da9e0801f59fef006769fe0a48d9c710f06
marcos-sco <marcos_sco@outlook.com>
2026-06-06 13:06:28 -0300

    refactor: add property casts for travel_zone, start_date, and end_date in Quote model

--- laravel/app/Models/Quote.php
+++ laravel/app/Models/Quote.php
@@ -2,12 +2,19 @@
 
 namespace App\Models;
 
+use App\Enums\TravelZone;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Quote extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'travel_zone' => TravelZone::class,
+        'start_date' => 'date',
+        'end_date' => 'date',
+    ];
 
     public function travelers()
     {
