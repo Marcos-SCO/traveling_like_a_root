524a2cf2b64ccee1ecfabb7d9b5a4bdebabed821
marcos-sco <marcos_sco@outlook.com>
2026-06-06 01:28:08 -0300

    refactor: ensure age is cast to int for accurate ageMultiplier calculations

--- laravel/app/Support/TravelerRateCalculator.php
+++ laravel/app/Support/TravelerRateCalculator.php
@@ -9,11 +9,11 @@ class TravelerRateCalculator
     public static function ageMultiplier(CarbonInterface $birthDate, CarbonInterface $tripStartDate): int|float
     {
 
-        $age = $birthDate->diffInYears($tripStartDate);
+        $age = (int) $birthDate->diffInYears($tripStartDate);
 
         return match (true) {
             $age <= 17.99 => 0.5,
-            $age >= 18 && $age <= 64.99 => 1,
+            $age >= 18.00 && $age <= 64.99 => 1,
             default => 2,
         };
     }
