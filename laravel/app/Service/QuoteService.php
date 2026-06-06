e8b6aa97fdcb482298c776bb4bcba6f50ffde7b4
marcos-sco <marcos_sco@outlook.com>
2026-06-06 16:38:57 -0300

    feat: refactor calculateTotal method for improved clarity and add formattedCalculatedArrayResponse method

--- laravel/app/Service/QuoteService.php
+++ laravel/app/Service/QuoteService.php
@@ -14,7 +14,8 @@ class QuoteService
 {
     public function __construct(private TravelerQuoteCalculator $travelerQuoteCalculator) {}
 
-    public function calculateTotal(array $data): array {
+    public function calculateTotal(array $data): array
+    {
         $travelZone = TravelZone::from(mb_strtolower($data['travel_zone']));
 
         $travelers = $data['travelers'];
@@ -27,24 +28,46 @@ class QuoteService
 
         $chargedDays = CalculateChargedDays::getDays($startDate, $endDate);
 
-        $travelersCost = $this->travelerQuoteCalculator->travelersCost($travelers, $travelZone, $startDate, $chargedDays);
+        $travelersCalculatedCostData = $this->travelerQuoteCalculator->travelersCost($travelers, $travelZone, $startDate, $chargedDays);
 
-        $totalGroupCost = $this->travelerQuoteCalculator->totalGroupCost($travelersCost);
+        $totalGroupCost = $this->travelerQuoteCalculator->totalGroupCost($travelersCalculatedCostData);
 
         $totalEnd = $totalGroupCost - ($totalGroupCost * $groupPercentageDiscount);
 
-        $travelersFormattedData =
-            $this->travelerQuoteCalculator->travelersFormattedData($travelersCost);
-
         $allWarningMessages =
-            $this->travelerQuoteCalculator->warningMessages($travelersCost);
+            $this->travelerQuoteCalculator->warningMessages($travelersCalculatedCostData);
 
         return [
+            'travel_zone' => $travelZone,
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+            'subtotal_amount' => $totalGroupCost,
+            
             'charged_days' => $chargedDays,
-            'travelers' => $travelersFormattedData,
+            
+            'travelers_calculated_cost_data' => $travelersCalculatedCostData,
+            
             'warnings' =>  $allWarningMessages,
+
+            'discount_amount' => $groupPercentageDiscount,
             'group_discount_percentage' => $groupPercentageDiscount * 100,
             'total_amount' => (int) round($totalEnd, 2),
+        ];
+    }
+
+    public function formattedCalculatedArrayResponse(array $data): array
+    {
+        $travelersFormattedData =
+            $this->travelerQuoteCalculator->travelersFormattedData($data['travelers_calculated_cost_data']);
+
+        return [
+            'charged_days' => $data['charged_days'],
+
+            'travelers' => $travelersFormattedData,
+
+            'warnings' =>  $data['warnings'],
+            'group_discount_percentage' => $data['group_discount_percentage'],
+            'total_amount' => $data['total_amount'],
         ];
     }
 }
