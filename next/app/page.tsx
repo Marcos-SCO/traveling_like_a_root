2ba7ecba8db07cc9f6d08d2b9dcaf65cfccf76f6
marcos-sco <marcos_sco@outlook.com>
2026-06-06 19:00:22 -0300

    fix: update header text in Home component to reflect correct title

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex-1 bg-slate-100">
       <div className="mx-auto max-w-5xl p-8">
-        <h1 className="mb-8 text-3xl font-bold">Cotações</h1>
+        <h1 className="mb-8 text-3xl font-bold">Traveling Like a Root</h1>
 
         <div className="space-y-8">
           <QuoteForm />
