5d5bc49fb8309817c995fd7deb4e2fe53a6f300d
marcos-sco <marcos_sco@outlook.com>
2026-06-06 01:22:39 -0300

    refactor: update ageMultiplier method to use float for age and improve age range checks

--- laravel/app/Support/TravelerRateCalculator.php
+++ laravel/app/Support/TravelerRateCalculator.php
@@ -1,16 +1,19 @@
-<?php 
+<?php
 
 namespace App\Support;
 
 use Carbon\CarbonInterface;
 
-class TravelerRateCalculator {
-    public static function ageMultiplier(CarbonInterface $birthDate, CarbonInterface $tripStartDate): float {
+class TravelerRateCalculator
+{
+    public static function ageMultiplier(CarbonInterface $birthDate, CarbonInterface $tripStartDate): int|float
+    {
+
         $age = $birthDate->diffInYears($tripStartDate);
 
-        return match(true) {
-            $age <= 17 => 0.5,
-            $age >= 18 && $age <= 64 => 1,
+        return match (true) {
+            $age <= 17.99 => 0.5,
+            $age >= 18 && $age <= 64.99 => 1,
             default => 2,
         };
     }
