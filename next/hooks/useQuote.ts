0eb6d3f60627782faf53a4aa39f8b5da23913119
marcos-sco <marcos_sco@outlook.com>
2026-06-07 00:33:05 -0300

    feat: integrate react-hot-toast for enhanced user feedback on quote submission

--- next/hooks/useQuote.ts
+++ next/hooks/useQuote.ts
@@ -1,21 +1,29 @@
+import toast from "react-hot-toast";
 import { createQuote } from "@/services/quoteService";
 import { useQuoteStore } from "@/store/quoteStore";
-import { QuoteRequest } from "@/types/quote";
+
+import { QuoteFormValues } from "@/schemas/quoteSchema";
 
 export function useQuote() {
     const { setQuote, setLoading } = useQuoteStore();
 
-    const submitQuote = async (payload: QuoteRequest) => {
+    const submitQuote = async (payload: QuoteFormValues) => {
         try {
             setLoading(true);
 
-            const response = await createQuote(payload);
+            const result = await createQuote(payload);
 
-            setQuote(response);
+            if (!(result.success)) {
+                toast.error(result.error);
+                return;
+            }
+            
+            // setQuote(result.data);
+            toast.success("Cotação gerada com sucesso!");
         } finally {
             setLoading(false);
         }
-    }
+    };
 
-    return { submitQuote }
+    return { submitQuote };
 }
